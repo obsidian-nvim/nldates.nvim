e5e30732d80a012d581ac2d4ca8cc3968d4e41f3
neo451 <412444506@qq.com>
2025-09-07 01:18:22 +0800

    feat: working!

---
+++ rplugin/node/nldates/index.js
@@ -0,0 +1,17 @@
+const chrono = require("chrono-node");
+const moment = require("moment");
+
+module.exports = (plugin) => {
+  plugin.registerFunction(
+    "NLDATE",
+    ([date, fmt]) => {
+      const results = chrono.parse(date);
+      if (results.length === 0) return;
+
+      const parsedDate = results[0].date();
+      const formatted = moment(parsedDate).format(fmt || "YYYY-MM-DD");
+      return formatted;
+    },
+    { sync: true },
+  );
+};
